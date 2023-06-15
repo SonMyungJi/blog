5ab8cf7e16edc723c29b928587a83f9c94d64919
Son MyungJi <linolousky@gmail.com>
2023-06-16 08:10:20 +0900

    3개로 나누기

--- src/main/java/com/sparta/blog/controller/BlogController.java
+++ src/main/java/com/sparta/blog/controller/BlogController.java
@@ -3,18 +3,15 @@ package com.sparta.blog.controller;
 import com.sparta.blog.dto.PostRequestDto;
 import com.sparta.blog.dto.PostResponseDto;
 import com.sparta.blog.entity.Post;
+import com.sparta.blog.service.BlogService;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -27,95 +24,34 @@ public class BlogController {
 
     @PostMapping("/post")
     public PostResponseDto createPost(@RequestBody PostRequestDto requestDto) {
-        Post post = new Post(requestDto);
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        String sql = "INSERT INTO post (title, author, contents, password) VALUES (?, ?, ?, ?)";
-        jdbcTemplate.update( con -> {
-                    PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-
-                    preparedStatement.setString(1, post.getTitle());
-                    preparedStatement.setString(2, post.getAuthor());
-                    preparedStatement.setString(3, post.getContents());
-                    preparedStatement.setInt(4, post.getPassword());
-                    return preparedStatement;
-                },
-                keyHolder);
-
-        Long id = keyHolder.getKey().longValue();
-        post.setId(id);
-        PostResponseDto postResponseDto = new PostResponseDto(post);
-
-        return postResponseDto;
+        BlogService blogService = new BlogService(jdbcTemplate);
+        return blogService.createPost(requestDto);
     }
 
     // 게시물 목록
     @GetMapping("/posts")
     public List<PostResponseDto> getPosts() {
-        String sql = "SELECT * FROM post";
-
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            Long id = rs.getLong("id");
-            String title = rs.getString("title");
-            String author = rs.getString("author");
-            String contents = rs.getString("contents");
-            int password = rs.getInt("password");
-            return new PostResponseDto(id, title, author, contents, password);
-        });
+        BlogService blogService = new BlogService(jdbcTemplate);
+        return blogService.getPosts();
     }
 
     // 게시글 조회
     @GetMapping("/post/{id}")
     public PostResponseDto getPost(@PathVariable Long id) {
-        String sql = "SELECT * FROM post WHERE id = ?";
-
-        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
-            String title = rs.getString("title");
-            String author = rs.getString("author");
-            String contents = rs.getString("contents");
-            int password = rs.getInt("password");
-            return new PostResponseDto(id, title, author, contents, password);
-        }, id);
+        BlogService blogService = new BlogService(jdbcTemplate);
+        return blogService.getPost(id);
     }
 
 
     @PutMapping("/post/{id}/{password}")
     public Long updateMemo(@PathVariable Long id, @PathVariable int password, @RequestBody PostRequestDto requestDto) {
-        Post post = findByIdAndPassword(id, password);
-        if (post != null) {
-            String sql = "UPDATE post SET title = ?, author = ?, contents = ? WHERE id = ?";
-            jdbcTemplate.update(sql, requestDto.getTitle(), requestDto.getAuthor(), requestDto.getContents(), id);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 글은 존재하지 않습니다.");
-        }
+        BlogService blogService = new BlogService(jdbcTemplate);
+        return blogService.updatePost(id, password, requestDto);
     }
 
     @DeleteMapping("/post/{id}/{password}")
     public Long deletePost(@PathVariable Long id, @PathVariable int password) {
-        Post post = findByIdAndPassword(id, password);
-        if(post != null) {
-            String sql = "DELETE FROM post WHERE id = ?";
-            jdbcTemplate.update(sql, id);
-
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 글은 존재하지 않습니다.");
-        }
-    }
-
-    private Post findByIdAndPassword(Long id, int password) {
-        String sql = "SELECT * FROM post WHERE id = ? AND password = ?";
-        return jdbcTemplate.query(sql, resultSet -> {
-            if(resultSet.next()) {
-                Post post = new Post();
-                post.setTitle(resultSet.getString("title"));
-                post.setAuthor(resultSet.getString("author"));
-                post.setContents(resultSet.getString("contents"));
-                post.setPassword(resultSet.getInt("password"));
-                return post;
-            } else {
-                return null;
-            }
-        }, id, password);
+        BlogService blogService = new BlogService(jdbcTemplate);
+        return blogService.deletePost(id, password);
     }
 }
--- src/main/java/com/sparta/blog/service/BlogService.java
+++ src/main/java/com/sparta/blog/service/BlogService.java
@@ -1,4 +1,64 @@
 package com.sparta.blog.service;
 
+import com.sparta.blog.dto.PostRequestDto;
+import com.sparta.blog.dto.PostResponseDto;
+import com.sparta.blog.entity.Post;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Service
+@RequestMapping("/api")
 public class BlogService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public BlogService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public PostResponseDto createPost(PostRequestDto requestDto) {
+        Post post = new Post(requestDto);
+
+        com.sparta.blog.service.BlogRepository blogRepository = new com.sparta.blog.service.BlogRepository(jdbcTemplate);
+        Post savePost = blogRepository.save(post);
+
+        PostResponseDto postResponseDto = new PostResponseDto(savePost);
+
+        return postResponseDto;
+    }
+
+    public List<PostResponseDto> getPosts() {
+        com.sparta.blog.service.BlogRepository blogRepository = new com.sparta.blog.service.BlogRepository(jdbcTemplate);
+        return blogRepository.findAll();
+    }
+
+    public PostResponseDto getPost(Long id) {
+        com.sparta.blog.service.BlogRepository blogRepository = new com.sparta.blog.service.BlogRepository(jdbcTemplate);
+        return blogRepository.find(id);
+    }
+
+    public Long updatePost(Long id, int password, PostRequestDto requestDto) {
+        com.sparta.blog.service.BlogRepository blogRepository = new com.sparta.blog.service.BlogRepository(jdbcTemplate);
+        Post post = blogRepository.findByIdAndPassword(id, password);
+        if (post != null) {
+            blogRepository.update(id, requestDto);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 글은 존재하지 않습니다.");
+        }
+    }
+
+    public Long deletePost(Long id, int password) {
+        com.sparta.blog.service.BlogRepository blogRepository = new com.sparta.blog.service.BlogRepository(jdbcTemplate);
+        Post post = blogRepository.findByIdAndPassword(id, password);
+        if(post != null) {
+            blogRepository.delete(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 글은 존재하지 않습니다.");
+        }
+    }
 }
